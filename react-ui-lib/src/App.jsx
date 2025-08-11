1516bca0ff50ddcf28c7add7a8d2735cebc887b1
LautaroFlores47 <lautaroflores47@gmail.com>
2025-08-11 02:13:21 -0300

    proyecto react con Material UI

---
+++ react-ui-lib/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from './components/Layout';
+import HomePage from './components/HomePage';
+import './EJ5estilos.css';
+
+function App() {
+  return (
+    <Layout>
+      <HomePage />
+    </Layout>
+  );
+}
+
+export default App;
+
+
+
