79692b4893286bf466214f51c33eec81e93c9cd4
LautaroFlores47 <lautaroflores47@gmail.com>
2025-10-23 19:30:41 -0300

    actualizo server.ts

--- CLASES/c21/backend/src/server.ts
+++ CLASES/c21/backend/src/server.ts
@@ -1,6 +1,9 @@
 import app from './app';
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+app.get('/health', (_req, res) => res.status(200).send('ok'));
+
 app.listen(PORT, () => {
-    console.log(`API escuchando en http://localhost:${PORT}`);
+    console.log(`✅ Servidor escuchando en puerto ${PORT}`);
 });
